2f35d2c22a1c2225a362584f826919e22fbc6a64
Juan Sebastian Vernaza Lopez <juandevelop85@gmail.com>
2026-09-16 21:51:49 -0500

    chore(user): dejar de versionar artefactos de build en user/target

--- user/target/x86_64-unknown-none/debug/build/hola/f47f4ce7e6a73749/out/hola.d
+++
@@ -1,5 +0,0 @@
-/Users/juandevelop/Develop/syso/user/target/x86_64-unknown-none/debug/build/hola/f47f4ce7e6a73749/out/hola.d: src/main.rs
-
-/Users/juandevelop/Develop/syso/user/target/x86_64-unknown-none/debug/build/hola/f47f4ce7e6a73749/out/hola: src/main.rs
-
-src/main.rs:
--- user/target/x86_64-unknown-none/debug/hola.d
+++
@@ -1,1 +0,0 @@
-/Users/juandevelop/Develop/syso/user/target/x86_64-unknown-none/debug/hola: /Users/juandevelop/Develop/syso/user/src/main.rs
